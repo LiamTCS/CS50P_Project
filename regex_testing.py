c7f9b1fca74718a1b62a0ec29db14e53121d4db4
Liam Sproule <liamsproule@outlook.com>
2022-12-18 12:49:01 +0000

    testing out how the regex page matching might work

---
+++ regex_testing.py
@@ -0,0 +1,27 @@
+import re
+
+
+
+sep_pos_string = "11000000001000"
+
+
+# desired output, tuples containing 
+
+
+
+
+def main():
+    
+    doc_list = subdoc_split(sep_pos_string)
+    print("Testing seperating the docs, using regex to find page ranges for docs")
+    print(f"The returned tuples are:\n{doc_list}")
+
+
+def subdoc_split(doc_contents):
+    
+    doc_page_list = tuple(re.finditer(r"[0]+", doc_contents))
+    return doc_page_list
+
+
+if __name__ == "__main__":
+    main()
