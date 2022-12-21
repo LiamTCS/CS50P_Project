7a93df7c20b3798611dc09ece5522ab49f1d1167
Liam Sproule <liamsproule@outlook.com>
2022-12-21 12:47:31 +0100

    added placeholder tests and imports

--- test_project.py
+++ test_project.py
@@ -2,7 +2,7 @@
     executable by pytest
 
 """
-from project import sub_doc_pos
+from project import sub_doc_pos, input_validation, QR_data
 
 import pytest
 import pickle
@@ -20,21 +20,30 @@ import pickle
 
 
 # TODO Input Validation - Passing Tests
+def test_input_validation_passing():
+    ...
+    
+    
 
 
 # TODO Input Validation - Failing Tests
 
+def test_input_validation_failing():
+    ...
 
 # TODO testing of pdf_2_image function, will have to investigate how to best test this, maybe with type?
+def test_pdf_2_image_list():
+    ...
 
-
-# TODO Testing of QR_seperator_present
-
+# TODO Testing of QR_sep_present
+def test_QR_sep_present():
+    ...
 
 # TODO Testing of QR_Data
+def test_QR_Data():
+    ...
 
 
-# TODO testing of sub_doc_pos
 def test_sub_doc_pos():
     list_1 = [True, False, False, False, False, False, False, True]
     list_2 = [True, False, False, True, False, False, False, False]
