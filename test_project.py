1c1ea9b7b867781630990e25e0d53aeb7c93a92e
Liam Sproule <liamsproule@outlook.com>
2022-12-21 12:38:16 +0100

    added test_sub_doc_pos

--- test_project.py
+++ test_project.py
@@ -2,9 +2,11 @@
     executable by pytest
 
 """
-
+from project import sub_doc_pos
 
 import pytest
+import pickle
+
 
 
 # TODO Add unit tests for each of the functions in project.py
@@ -33,6 +35,19 @@ import pytest
 
 
 # TODO testing of sub_doc_pos
+
+
+def test_sub_doc_pos():
+    list_1 = [True, False, False, False, False, False, False, True]
+    list_2 = [True, False, False, True, False, False, False, False]
+    list_3 = [False, False, False]  # no sep pages
+    list_4 = [True, False, False, True, False,
+              False, True, True, False, False, False]
+
+    assert sub_doc_pos(list_1) == [(1, 7)]
+    assert sub_doc_pos(list_2) == [(1, 3), (4, 8)]
+    assert sub_doc_pos(list_3) == [(0, 3)]
+    assert sub_doc_pos(list_4) == [(4, 6), (1, 3), (8, 11)]
 
 # TODO testing of pdf_split
 
